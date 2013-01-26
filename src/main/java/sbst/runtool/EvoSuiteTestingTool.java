3c8529b96fc38abb4022107594e483f2af6c4655
gordon <devnull@localhost>
2013-01-26 08:36:17 +0000

    Add empty skeleton class

---
+++ src/main/java/sbst/runtool/EvoSuiteTestingTool.java
@@ -0,0 +1,26 @@
+package sbst.runtool;
+
+import java.io.File;
+import java.util.List;
+
+public class EvoSuiteTestingTool implements ITestingTool {
+
+	@Override
+	public List<File> getExtraClassPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void initialize(File src, File bin, List<File> classPath) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run(String cName) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
