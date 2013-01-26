b4325218810194852835c00c6e6974493a4953e6
gordon <devnull@localhost>
2013-01-26 08:46:38 +0000

    Add untested implementation

--- src/main/java/sbst/runtool/EvoSuiteTestingTool.java
+++ src/main/java/sbst/runtool/EvoSuiteTestingTool.java
@@ -1,26 +1,53 @@
 package sbst.runtool;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import org.evosuite.EvoSuite;
 
 public class EvoSuiteTestingTool implements ITestingTool {
 
+	private EvoSuite evosuite = null;
+	
+	private String targetClassPath ="";
+	
 	@Override
 	public List<File> getExtraClassPath() {
-		// TODO Auto-generated method stub
-		return null;
+		// TODO export evosuite.jar?
+		return new ArrayList<File>();
 	}
 
 	@Override
 	public void initialize(File src, File bin, List<File> classPath) {
-		// TODO Auto-generated method stub
-
+		StringBuilder sb = new StringBuilder();
+		sb.append(src.getPath());
+		for(File dependency : classPath) {
+			sb.append(File.pathSeparator);
+			sb.append(dependency.getPath());
+		}
+		targetClassPath = sb.toString();
+		evosuite = new EvoSuite();
 	}
 
 	@Override
 	public void run(String cName) {
-		// TODO Auto-generated method stub
-
+		if(evosuite == null) {
+			System.err.println("EvoSuite has not been initialized with the target classpath!");
+			return;
+		}
+		
+		List<String> commands = new ArrayList<String>();
+		commands.addAll(Arrays.asList(new String[] {
+		        "-generateSuite",
+		        "-class",
+		        cName,
+		        "-Dshow_progress=false", "-Dstopping_condition=MaxTime",
+		        "-sandbox", "-Dtest_comments=false", "-cp="+targetClassPath }));
+		String[] command = new String[commands.size()];
+		commands.toArray(command);
+		evosuite.parseCommandLine(command);
 	}
 
 }
